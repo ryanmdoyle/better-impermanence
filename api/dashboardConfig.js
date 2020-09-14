c14bcbed4268075865c766408f8bbe2f10f42b09
Ryan Doyle <doylecodes@gmail.com>
2020-09-13 21:02:01 -0700

    add netlify deploy

--- api/dashboardConfig.js
+++ api/dashboardConfig.js
@@ -1,18 +1,18 @@
-// export default {
-//   widgets: [
-//     {
-//       name: 'netlify',
-//       options: {
-//         title: 'My Netlify deploys',
-//         sites: [
-//           {
-//             title: 'Website',
-//             apiId: 'b8a9673a-3c61-4cc1-9825-c10945d05914',
-//             buildHookId: '5e2635290d66e51b5e2dad68',
-//             name: 'joemartone'
-//           }
-//         ]
-//       }
-//     }
-//   ]
-// }
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'Better Impermanence Site Deployment',
+        sites: [
+          {
+            title: 'Better Impermanence',
+            apiId: '525a6b53-504e-44ab-94d6-0abd6087ff27',
+            buildHookId: '5f5ee9dec3abcb21b3cbb846',
+            name: 'betterimpermanence'
+          }
+        ]
+      }
+    }
+  ]
+}
